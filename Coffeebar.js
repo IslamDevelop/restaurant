8db615531f8f40ea62155c15825a671f7920aa77
magomaevislam <islam.magomaev@mail.ru>
2024-08-18 20:11:07 +0300

    1.3

--- Coffeebar.js
+++ Coffeebar.js
@@ -1,14 +1,15 @@
+import contentHome from "./menu"
+
 export class Coffebar {
-constructor() {
-   this.bar = []
-}
-addCoffee(coffee) {
-    this.bar.push(coffee)
+ constructor() {
+     this.bar = []
+    }
+    
+    addCoffee(coffee) {
+        this.bar.push(coffee)
 }
 removeCoffee(index) {
     this.bar.splice(index, 1)
 }
-displayCoffee() {
-    
-}
+
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /.(png|jpe?g|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   mode: "development",
